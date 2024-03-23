9fd73facc8b9312bc47d5133928f5bb45ff2abd2
Joseph Mearman <joseph@mearman.co.uk>
2024-03-23 20:44:23 +0000

    Add keys and values functions to object utility kit

--- packages/utility-kit/src/nodes/object/index.ts
+++ packages/utility-kit/src/nodes/object/index.ts
@@ -1,3 +1,6 @@
 export { entries } from "./entries.js";
 export { fromEntries } from "./fromEntries.js";
+export { keys } from "./keys.js";
+export { rest } from "./rest.js";
 export { spread } from "./spread.js";
+export { values } from "./values.js";
