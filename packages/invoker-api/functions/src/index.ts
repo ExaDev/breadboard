44b938310dbe33ba3e05cb26bac97fdb0c1c42c5
Joseph Mearman <joseph@mearman.co.uk>
2024-04-15 16:03:58 +0100

    WIP

--- packages/invoker-api/functions/src/index.ts
+++ packages/invoker-api/functions/src/index.ts
@@ -1,32 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import * as logger from "firebase-functions/logger";
-// import { onRequest } from "firebase-functions/v2/https";
-
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", { structuredData: true });
-//   response.send("Hello from Firebase!");
-// });
-
-// const root = onRequest((request, response) => {
-//   response.send("Hello from Firebase!");
-// });
-// export default root;
-
-import { initializeApp } from "firebase-admin/app";
-import { https } from "firebase-functions/v2";
+import { type HttpFunction } from "@google-cloud/functions-framework";
+import express from "express";
 import { expressApp } from "./app/index.js";
 
-initializeApp();
+export const api: HttpFunction = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  return expressApp(req, res);
+};
 
-export const api = https.onRequest(expressApp);
+export default api;
