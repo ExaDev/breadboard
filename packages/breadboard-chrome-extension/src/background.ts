649a58548e138f2119eb4d9ca35cb9dcda5ec5ec
a.mitu <a.mitu@sourcepulp.com>
2024-04-23 13:01:48 +0100

    use crxjs plugin in the vite config and replace manifest json with config file using defineManifest

---
+++ packages/breadboard-chrome-extension/src/background.ts
@@ -0,0 +1,3 @@
+console.log(
+  "Hello from background script. This should appear in the extension's dev console."
+);
---
+++ packages/breadboard-chrome-extension/src/content.tsx
@@ -0,0 +1,3 @@
+console.log(
+  "Hello from content script in main console. This should appear in the dev console for a specific tab."
+);
