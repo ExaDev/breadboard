99501f1f781d7291c5ed3c4a0769ec9f503b35ff
a.mitu <a.mitu@sourcepulp.com>
2024-05-21 11:03:00 +0100

    wip: new settings popup

--- packages/breadboard-chrome-extension/src/pages/settings/index.tsx
+++ packages/breadboard-chrome-extension/src/pages/settings/index.tsx
@@ -1,9 +1,9 @@
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import BoardSettings from "./components/BoardSettings";
+import ReactDOM from "react-dom/client";
 
 const container = document.createElement("div");
 container.setAttribute("class", "settings");
 document.body.appendChild(container);
-const root = createRoot(container);
-root.render(BoardSettings());
+const root = ReactDOM.createRoot(container);
+root.render(<BoardSettings />);
