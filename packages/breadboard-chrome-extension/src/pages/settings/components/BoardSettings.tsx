579990ff61ff179d64cc37f2a868130a094ec303
a.mitu <a.mitu@sourcepulp.com>
2024-05-24 10:01:57 +0100

    update Settings

--- packages/breadboard-chrome-extension/src/pages/settings/components/BoardSettings.tsx
+++ packages/breadboard-chrome-extension/src/pages/settings/components/BoardSettings.tsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
+import { faPlus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import React, { useCallback, useState } from "react";
+import { IconButton } from "@/components/IconButton";
 
 const BoardSettings = (): React.JSX.Element => {
   //References to inputs created below as JSX
   const nameRef = React.createRef<HTMLInputElement>();
   const valueRef = React.createRef<HTMLInputElement>();
   const statusRef = React.createRef<HTMLDivElement>();
-  //const taskRef = React.createRef<HTMLSelectElement>();
-  const [showSettingsGroup, setShowSettingsGroup] = useState<boolean>(false);
+  const [showSettingsGroup, setShowSettingsGroup] = useState<boolean>(true);
+  const [secretsToAdd, setSecretsToAdd] = useState<
+    { name: string; value: string }[]
+  >([]);
 
   const saveKey = () => {
-    const name = nameRef.current?.value;
-    const value = valueRef.current?.value;
-    if (name && value) {
+    secretsToAdd.map(({ name, value }) => {
       chrome.storage.sync.set({ [name]: value }, () => {
         // Update status to let user know options were saved.
         console.log(name, value);
@@ -23,43 +25,100 @@ const BoardSettings = (): React.JSX.Element => {
           }, 750);
         }
       });
-    }
+    });
   };
 
-  /* const saveModel = () => {
-    const model = taskRef.current?.value;
-    chrome.storage.sync.set({ model: model }, () => {
-      // Update status to let user know options were saved.
-      const status = statusRef.current;
-      if (status) {
-        status.textContent = "Claude summarisation model saved.";
-        setTimeout(() => {
-          status.textContent = "";
-        }, 750);
+  const secretsRow = () => {
+    return (
+      <>
+        <input ref={nameRef} type="text" />
+        <input ref={valueRef} type="password" />
+        <IconButton
+          onClick={() =>
+            onAddClick(
+              nameRef.current?.value ?? "",
+              valueRef.current?.value ?? ""
+            )
+          }
+          Icon={faPlus}
+        />
+      </>
+    );
+  };
+
+  const onAddClick = useCallback(
+    (name: string, value: string) => {
+      if (secretsToAdd.findIndex((val) => val.name === name) === -1) {
+        secretsToAdd.push({ name, value });
+        setSecretsToAdd(Object.assign([], secretsToAdd));
       }
-    });
-  }; */
+    },
+    [secretsToAdd, setSecretsToAdd]
+  );
+
+  const onRemoveClick = useCallback(
+    (keyToDelete: string) => {
+      return () => {
+        const filteredEmails = secretsToAdd.filter(
+          (val) => val.name !== keyToDelete
+        );
+        setSecretsToAdd(filteredEmails);
+      };
+    },
+    [secretsToAdd, setSecretsToAdd]
+  );
 
   return (
-    <ul className="settingsGroup">
-      <li>
-        <button
-          className="settingGroupName"
-          onClick={() => setShowSettingsGroup(true)}
-        >
-          Secrets
+    <>
+      <ul className="settingsGroup">
+        <li className="settingsGroupCategory">
+          /* TODO: Create custom button component with styles from the class
+          'settingsGroupName' */
+          <button className="settingGroupName">General</button>
+          <button
+            className="settingGroupName"
+            onClick={() => setShowSettingsGroup(true)}
+          >
+            Secrets
+          </button>
+          <button className="settingGroupName">Boards</button>
+          <button className="settingGroupName">Saved Outputs</button>
+        </li>
+        {showSettingsGroup ? (
+          <>
+            <section className="settingsGroupCategoryItems">
+              <p>
+                This is where you can set secrets as key-value pairs. For
+                example, you can add a Claude API key for performing text
+                summarisation.
+              </p>
+              <div className="secretFields">{secretsRow()}</div>
+              <div>
+                {secretsToAdd.map((nameAndValue) => (
+                  <div>
+                    <span>
+                      {nameAndValue.name} - {nameAndValue.value}
+                    </span>
+                    <IconButton
+                      Icon={faTrash}
+                      onClick={onRemoveClick(nameAndValue.name)}
+                    />
+                  </div>
+                ))}
+              </div>
+            </section>
+          </>
+        ) : null}
+      </ul>
+      <footer>
+        <button className="saveButton" onClick={saveKey}>
+          Save
         </button>
-      </li>
-      {showSettingsGroup ? (
-        <section className="settingsGroupItems">
-          <label htmlFor="nameRef">Name</label>
-          <input ref={nameRef} type="text" />
-          <label htmlFor="valueRef">Value</label>
-          <input ref={valueRef} type="password" />
-          <button onClick={saveKey}>Save</button>
-        </section>
-      ) : null}
-    </ul>
+        <button className="cancelButton" onClick={(): void => {}}>
+          Cancel
+        </button>
+      </footer>
+    </>
   );
 };
 
