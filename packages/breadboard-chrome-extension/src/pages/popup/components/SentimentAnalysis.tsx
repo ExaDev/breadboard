948e657bcbb8909cd34b0c2b5f6a7c4f453a85c9
a.mitu <a.mitu@sourcepulp.com>
2024-05-30 15:54:19 +0100

    add text classification board

--- packages/breadboard-chrome-extension/src/pages/popup/components/SentimentAnalysis.tsx
+++ packages/breadboard-chrome-extension/src/pages/popup/components/SentimentAnalysis.tsx
@@ -1,41 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { PacmanLoader } from "react-spinners";
-import sentimentAnalysisBoard from "@breadboard/boards/sentiment-analysis";
 import useActiveTab from "../../../chrome-api-hooks/use-active-tab";
 import useTextSelection from "../../../chrome-api-hooks/use-text-selection";
+import { ExtensionBoardRunner } from "@/breadboard/classes/ExtensionBoardRunner";
+import serializedSentimentBoard from "../../../breadboard/graphs/sentimentBoard.json";
 
 const SentimentAnalysis = (): React.JSX.Element => {
   const [output, setOutput] = useState<React.ReactNode | undefined>(undefined);
   const [loading, setLoading] = useState<boolean>(false);
+  const [key, setKey] = useState<string>("");
 
-  const runBoard = async (message: string | undefined) => {
-    const board = await sentimentAnalysisBoard({
-      message: message,
+  useEffect(() => {
+    chrome.storage.sync.get(["HUGGING_FACE_API_KEY"], (result) => {
+      setKey(result["HUGGING_FACE_API_KEY"]);
     });
-    return board;
-  };
+  }, [key]);
 
-  const handleSelection = async () => {
-    const activeTabId = (await useActiveTab()).id;
-    const result = useTextSelection(activeTabId ?? 0);
+  const handleSelectionSentiment = async () => {
+    const activeTab = await useActiveTab();
+    const result = await useTextSelection(activeTab.id ?? 0);
     setLoading(true);
-    const boardRun = await runBoard(await result);
-    console.log(JSON.stringify(boardRun["output"], null, 2));
-    setOutput(JSON.stringify(boardRun["output"], null, 2));
+    const extensionRunner = new ExtensionBoardRunner(serializedSentimentBoard, []);
+    const boardRun = await extensionRunner.runBoard({
+		inputs: result,
+		apiKey: key,
+		use_cache: true,
+		wait_for_model: false
+    });
+	console.log(boardRun)
+    if (boardRun) setOutput(boardRun["output"] as React.ReactNode);
     setLoading(false);
-    /* const message = {
-      text: boardRun["output"],
-    };
-    chrome.runtime.sendMessage(message, async (response) => {
-      console.log(response);
-      setOutput(JSON.stringify(await response, null, 2) as React.ReactNode);
-    }); */
   };
 
   return (
     <main>
       <section className="summarisationForm">
-        <button className="submitSelected" onClick={handleSelection}>
+        <button className="submitSelected" onClick={handleSelectionSentiment}>
           Get sentiment for selected text
         </button>
       </section>
