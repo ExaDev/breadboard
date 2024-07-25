262e47384b4032aa561705c5b49ea9e502497cc2
jig925 <57284752+jig925@users.noreply.github.com>
2024-07-25 16:02:31 +0100

    Build Syntax
    Updated board for each to use new build api syntax

--- packages/example-boards/src/boards/playground/board-for-each.ts
+++ packages/example-boards/src/boards/playground/board-for-each.ts
@@ -1,184 +1,87 @@
-import {
-  AbstractNode,
-  base,
-  board,
-  code,
-  InputValues,
-  NodeValue,
-} from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import {
-  AbstractValue,
-  NodeProxy,
-} from "../../../../breadboard/dist/src/new/grammar/types";
-import { OutputValue } from "../../../../breadboard/dist/src/new/runner/types";
-import { pop } from "../../utils/pop";
+/**
+ * @license
+ * Copyright 2024 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-const demoBoard = await board<
-  {
-    object: string;
-  },
-  {
-    item: string;
+import { board, converge, input, loopback, output } from "@breadboard-ai/build";
+import { code, invoke, passthrough } from "@google-labs/core-kit";
+
+// TODO have a board and then serialize it as a default
+export const boardJSON = input({
+  type: "unknown",
+  title: "board json",
+  default: {
+    "edges": [
+      { "from": "fn-4", "to": "output-5", "out": "*", "in": "" },
+      { "from": "input-3", "to": "fn-4", "out": "object", "in": "object" }],
+    "nodes": [
+      { "id": "output-5", "type": "output", "configuration": {}, "metadata": { "title": "Output" } },
+      { "id": "fn-4", "type": "runJavascript", "configuration": { "code": "const fn_4 = inputs=>{const{object}=inputs;return{object:object.split(\"\").map(c=>c===c.toUpperCase()?c.toLowerCase():c.toUpperCase()).join(\"\")}};", "name": "fn_4", "raw": true } },
+      { "id": "input-3", "type": "input", "configuration": { "schema": { "type": "object", "properties": { "object": { "type": "string", "title": "object" } }, "required": ["object"] } }, "metadata": { "title": "Input" } }], "graphs": {}
   }
->(() => {
-  const input = base.input({
-    $metadata: {
-      title: "Input",
-    },
-  });
-
-  const reverse = code<{ object: string }, { object: string }>((inputs) => {
-    const { object } = inputs;
-    return {
-      object: object
-        .split("")
-        .map((c) => (c === c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()))
-        .join(""),
-    };
-  })(input.object);
-
-  const output = base.output({
-    $metadata: {
-      title: "Output",
-    },
-  });
-  reverse.to(output);
-  return output;
-}).serialize();
-const input = base.input({
-  $metadata: {
-    title: "Input",
-  },
-  schema: {
-    type: "object",
-    properties: {
-      board: {
-        examples: [JSON.stringify(demoBoard)],
-      },
-      array: {
-        type: "array",
-        examples: [JSON.stringify(["The", "quick", "brown", "fox"])],
-      },
-    },
-  },
-});
-
-const passthrough = core.passthrough({
-  $metadata: {
-    title: "Passthrough",
-  },
-  board: input.board,
-});
-passthrough.board.to(passthrough);
-
-const invocation = core.invoke({
-  // $board: input.board,
-  object: passthrough.item,
-  $board: passthrough.board,
-  $metadata: {
-    title: "Invoke",
-  },
-});
-
-const output = base.output({
-  $metadata: {
-    title: "Output",
-  },
-});
-
-const popItem = pop({
-  $metadata: {
-    title: "Pop item",
-  },
-  array: input.array as unknown as [],
-});
-
-popItem.array.to(popItem);
-popItem.item.to(passthrough);
-
-type AccumulatorInput<T> = {
-  item: T;
-  array?: T[];
-};
-type AccumulatorOutput<T> = {
-  array: T[];
-};
-
-function accumulator<T>(
-  inputs:
-    | AbstractNode<InputValues, AccumulatorInput<T>>
-    | AbstractValue<NodeValue>
-    | Partial<{
-        [K in keyof AccumulatorInput<T>]:
-          | AbstractValue<AccumulatorInput<T>[K]>
-          | NodeProxy<InputValues, OutputValue<AccumulatorInput<T>[K]>>
-          | AccumulatorInput<T>[K];
-      }>
-    | {
-        [p: string]:
-          | AbstractValue<NodeValue>
-          | NodeProxy<InputValues, Partial<InputValues>>
-          | NodeValue;
-      }
-): NodeProxy<AccumulatorInput<T>, Required<AccumulatorOutput<T>>> {
-  return code<AccumulatorInput<T>, AccumulatorOutput<T>>(
-    (inputs: AccumulatorInput<T>) => {
-      const { item, array = [] } = inputs;
-      return { array: [...array, item] };
-    }
-  )(inputs);
-}
-
-const accumulate = accumulator({
-  item: invocation.object,
-  $metadata: {
-    title: "Accumulate",
-  },
-});
-
-accumulate.to(accumulate);
-
-function emitter<T>(
-  args: any
-): NodeProxy<
-  { a?: T[] | undefined; b?: T[] | undefined },
-  Required<{ emit?: T[] | undefined }>
-> {
-  return code<
-    { a?: T[]; b?: T[] },
-    {
-      emit?: T[];
-    }
-  >(
-    (
-      inputs
-    ): { emit: T[] | undefined; a: T[] | undefined; b: T[] | undefined } => {
-      let emit = undefined;
-      if (!inputs.a || inputs.a.length === 0) {
-        emit = inputs.b;
-      }
-
-      return { emit: emit, a: inputs.a, b: inputs.b };
-    }
-  )(args);
-}
-
-emitter({
-  a: popItem.array,
-  b: accumulate.array,
-  $metadata: {
-    title: "Emitter",
-  },
 })
-  .emit.as("outputs")
-  .to(output);
 
-const serialised = await input.serialize({
-  title: "Board for Each",
-  description:
-    "Iterate over an array and run a subgraph for each item in the array.",
-});
+const arrayInput = input({
+  type: "unknown",
+  title: "array input",
+  default: ["What", "did", "the", "fox", "say?"]
+})
 
-export { serialised as graph, input, output };
-export default serialised;
+const itemLoop = loopback({ type: "unknown" })
+
+// converge so we can provide an initial input but also cycle on this node with the next array
+const pop = code({$id:"Pop", array: converge(arrayInput, itemLoop) }, { array: "unknown", item: "unknown" }, ({ array }) => {
+  // @ts-ignore
+  const [item, ...rest] = array;
+
+  if (item) {
+    return { array: rest, item: item } as any;
+  }
+  return {} as any;
+})
+
+const boardLoop = loopback({ type: "unknown" })
+
+// @ts-ignore
+// passthrough complains because of converge()
+const passthroughOutput = passthrough({ board: converge(boardJSON, boardLoop), item: pop.outputs.item, array: pop.outputs.array });
+// start the cycle again until array is empty
+boardLoop.resolve(passthroughOutput.outputs.board)
+itemLoop.resolve(passthroughOutput.outputs.array)
+
+
+const res = invoke({
+  $id: "Board Output",
+  $board: passthroughOutput.outputs.board,
+  object: passthroughOutput.outputs.item
+}).unsafeOutput("object");
+
+
+const arrayLoop = loopback({type:"unknown"})
+
+const accummulate = code({$id: "Accummulate", item: res, array: converge([] , arrayLoop) }, {array: "unknown"}, ({ item, array}) => {
+  
+  return { array: [...array as [] , item] } as any;
+}).outputs.array
+
+arrayLoop.resolve(accummulate)
+
+const emitter = code({$id:"Emitter", a: pop.outputs.array, b: accummulate}, {emit: "unknown", a:"unknown", b:"unknown"}, ({a, b}) => {
+  let emit = undefined;
+  // @ts-ignore
+  if (!a || a.length === 0) {
+    emit = b;
+  }
+
+  return { emit: emit } as any;
+})
+
+
+export default board({
+  title: "Board for each",
+  version: "0.1.0",
+  inputs: { board: boardJSON, array: arrayInput },
+  outputs: { outputs: output(emitter.outputs.emit)}
+})
+
