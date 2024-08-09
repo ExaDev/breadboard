00bec5a1e24f803fbc6020a51a567de830260530
jig925 <57284752+jig925@users.noreply.github.com>
2024-08-09 15:04:21 +0100

    WIP
    Migrated tour-guide-writer board to build api

--- packages/example-boards/src/boards/playground/tour-guide-writer.ts
+++ packages/example-boards/src/boards/playground/tour-guide-writer.ts
@@ -1,58 +1,19 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
 
-import { Schema, V, base, board, code } from "@google-labs/breadboard";
-import { core } from "@google-labs/core-kit";
-import { templates } from "@google-labs/template-kit";
 
-const metadata = {
-  title: "Tour Guide Writer",
-  description:
-    "This boards attempts to write a tour guide for a specified location. This time, it takes a text generator board as an input.\n\nInterestingly, because we currently don't have a way to close over the inputs of the invoked text generator, this board exposes the text generator's inputs as its own inputs.",
-  version: "0.0.3",
-};
 
-const inputSchema = {
-  type: "object",
-  properties: {
-    location: {
-      type: "string",
-      title: "Location",
-      description: "The location for which to write a tour guide",
-      examples: ["Stresa, Italy"],
-    },
-    generator: {
-      type: "board",
-      title: "Text Generator",
-      description: "The text generator to use for writing the tour guide",
-      examples: ["text-generator.json"],
-    },
-  },
-  required: ["location"],
-} satisfies Schema;
+import { annotate, array, board, converge, input, loopback, object, output } from "@breadboard-ai/build"
 
-const outputSchema = {
-  type: "object",
-  properties: {
-    guide: {
-      type: "string",
-      title: "Guide",
-      description: "The tour guide for the specified location",
-      format: "markdown",
-    },
-  },
-} satisfies Schema;
+import { invoke, code, passthrough } from "@google-labs/core-kit";
+import { prompt, promptPlaceholder } from "@google-labs/template-kit";
 
-const graph = board(() => {
-  const parameters = base.input({ $id: "parameters", schema: inputSchema });
-
-  const output = base.output({ $id: "guide", schema: outputSchema });
-
-  const travelItinerary = templates.promptTemplate({
-    template: `[Place] Seattle, WA
+const location = input({ title: "location", type: "string", default: "Stresa, Italy" })
+const boardLocation = input({
+  title: "Board Location", type: annotate(object({}), {
+    behavior: ["board"],
+  }), default: ["text-generator.json"]
+})
+// THIS MIGRATION DOES NOT USE CORE.CURRY OR CORE.MAP AS THEY HAVE NOT YET BEEN MIGRATED
+const itenaryTemplate = prompt`[Place] Seattle, WA
   [Top ten place-based experiences with no duplicates]
   1) See the city from the Space Needle
   2) Watch the fish throwing at Pike Place Market
@@ -91,38 +52,50 @@ const graph = board(() => {
   9) Shop at the Water Tower Place
   10) See the Chicago Theatre
 
-  [Place] {{location}}
+  [Place] ${promptPlaceholder(location, { name: "location" })}
   [Top ten place-based experiences with no duplicates]
-  `,
-    $id: "travelItinerary",
-    location: parameters.location,
-  });
+  `.configure({ id: "travelTininerary" })
 
-  const travelItineraryGenerator = core.invoke({
-    $id: "travelItineraryGenerator",
-    stopSequences: ["\n[Place]"],
-    $board: parameters.generator as V<string>,
-    useStreaming: false,
-  });
 
-  travelItineraryGenerator.text.as("itinerary").to(output);
+const travelItinerary = invoke({$id:"Travel Itenary", $board: boardLocation, text: itenaryTemplate, stopSequences: ["\n[Place]"], useStreaming: false, }).unsafeOutput("text")
 
-  type Itinerary = { itinerary: string };
-  type SplitItinerary = { list: string[] };
+const splitItinerary = code({ itenary: travelItinerary }, { list: array("string") }, ({ itenary }) => {
+  const input = itenary as unknown as string
+  const list = input
+    .split(/[0-9]{1,2}\)/)
+    .map((e) => e.trim())
+    .filter((e) => e !== "");
+  return { list } as any;
+})
 
-  const splitItinerary = code<Itinerary, SplitItinerary>(({ itinerary }) => {
-    const list = itinerary
-      .split(/[0-9]{1,2}\)/)
-      .map((e) => e.trim())
-      .filter((e) => e !== "");
-    return { list };
-  })({ itinerary: travelItineraryGenerator.text as V<string> });
+const popLoopBack = loopback({ type: array("string") })
 
-  travelItinerary.prompt.as("text").to(travelItineraryGenerator);
+const pop = code({ $id: "Pop", array: converge(splitItinerary.outputs.list, popLoopBack) }, { array: array("string"), item: "string" }, ({ array }) => {
+  const [item, ...rest] = array;
+  if (item) {
+    return { array: rest, item: item } as any;
+  }
+  return {} as any;
+})
 
-  const createGuide = board(({ location, generator }) => {
-    const guideTemplate = templates.promptTemplate({
-      template: `[City] Paris, France
+const boardLoop = loopback({
+  type: annotate(object({}), {
+    behavior: ["board"],
+  }),
+})
+
+const locationLoop = loopback({ type: "string" })
+const itemLoopBack = loopback({ type: "string" })
+itemLoopBack.resolve(pop.outputs.item)
+popLoopBack.resolve(pop.outputs.array)
+
+// @ts-ignore
+const passthroughOutput = passthrough({ board: converge(boardLocation, boardLoop), item: pop.outputs.item, array: pop.outputs.array, location: converge(location, locationLoop) });
+
+locationLoop.resolve(passthroughOutput.outputs.location)
+boardLoop.resolve(passthroughOutput.outputs.board)
+
+const guideTemplate = prompt`[City] Paris, France
     [Activity] Have a picnic in the Luxembourg Gardens
     [Experiential story] Grab a baguette, some cheese and bottle of wine and head over to Luxembourg Gardens. You'll enjoy an even stroll, a great chance to people watch, and a charming free evening that is quintessentially Parisian.
 
@@ -135,65 +108,48 @@ const graph = board(() => {
     [Experiential story] A pod race is a race of flying engines called pods. Pod racing is a dangerous sport and was very popular in the Outer Rim Territories before the Empire was formed.
 
 
-    [City] {{location}}
-    [Activity] {{activity}}
+    [City] ${promptPlaceholder(passthroughOutput.outputs.location, { name: "location" })}
+    [Activity] ${promptPlaceholder(passthroughOutput.outputs.item, { name: "activity" })}
     [Experiential story]
-    `,
-      $id: "guideTemplate",
-      location: location,
-      activity: base.input({}).item.as("activity"),
-    });
+    `.configure({ id: "guideTemplate" })
 
-    const guideGenerator = core.invoke({
-      $id: "guideGenerator",
-      stopSequences: ["\n[City]"],
-      $board: generator as V<string>,
-      useStreaming: false,
-    });
 
-    guideTemplate.prompt.as("text").to(guideGenerator);
-    return guideGenerator.text.as("guide").to(base.output({}));
-  });
+const guideOutput = invoke({$id:"Guide", $board: converge(boardLocation, boardLoop), text: guideTemplate.outputs.prompt, stopSequences: ["\n[Place]"], useStreaming: false, }).unsafeOutput("text")
+const arrayLoop = loopback({ type: array("string") })
 
-  const guideWithParameters = core.curry({
-    $metadata: { title: "Curry Params into Guide" },
-    $board: createGuide,
-    location: parameters.location,
-    generator: parameters.generator,
-  });
+// get output for each activity 
+const accummulate = code({ $id: "Accummulate", item: guideOutput, array: converge([], arrayLoop) }, { array: array("string") }, ({ item, array }) => {
+  return { array: [...array as [], item] } as any;
+}).outputs.array
 
-  const createGuides = core.map({
-    $id: "createGuides",
-    board: guideWithParameters.board,
-    list: splitItinerary.list,
-  });
+arrayLoop.resolve(accummulate)
 
-  type GuideMaterials = {
-    location: string;
-    activities: string[];
-    guides: Record<string, { guide: string }>[];
-  };
+// return results only when popped array is empty
+const emitter = code({ $id: "Emitter", poppedArray: pop.outputs.array, accumulate: accummulate }, { emit: array("string"), a: array("string"), b: array("string") }, ({ poppedArray, accumulate }) => {
+  let emit = undefined;
 
-  type Guide = { guide: string };
+  if (!poppedArray || poppedArray.length === 0) {
+    // @ts-ignore
+    emit = accumulate;
+  }
 
-  const combineGuides = code<GuideMaterials, Guide>(
-    ({ location, activities, guides }) => {
-      const guideList = guides.map((item) => item.guide);
-      return {
-        guide: `# ${location}\n${activities
-          .map((activity, index) => `## ${activity}\n${guideList[index]}\n\n`)
-          .join("")}`,
-      };
-    }
-  )({
-    location: parameters.location as V<string>,
-    activities: splitItinerary.list,
-    guides: createGuides.list as V<GuideMaterials["guides"]>,
-  });
+  // board wont return until output is set, so if it's undefined we will keep looping until the array is empty
+  return { emit: emit } as any;
+})
 
-  combineGuides.guide.to(output);
+const combineGuides = code({$id:"combineGuide", location: location, itenary: splitItinerary.outputs.list, guides: emitter.outputs.emit }, {guide: "string"}, ({location, itenary, guides}) => {
+  const guideList = guides.map((item) => item);
+  return {
+    guide: `# ${location}\n${itenary
+      .map((activity, index) => `## ${activity}\n${guideList[index]}\n\n`)
+      .join("")}`,
+  } as any;
+})
 
-  return output;
-});
 
-export default await graph.serialize(metadata);
+export default board({
+  title: "Tour Guide Writer",
+  version: "0.1.0",
+  inputs: { location, boardLocation },
+  outputs: { output: output(combineGuides.outputs.guide), itenary:(travelItinerary) }
+})
